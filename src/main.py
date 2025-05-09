4a9343f5145e53c0483949314af4cb71fc11a638
tims-exe <timonkjohn@gmail.com>
2025-05-09 20:58:58 +0530

    logging

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+# from .database.core import engine, Base
+# from .entities.todo import Todo
+# from .entities.User import User
+# from .api import register_routes
+
+app = FastAPI()
+
